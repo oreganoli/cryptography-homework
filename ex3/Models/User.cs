c5d50438007737b7cf87b7433938c024d2d3b5ab
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-03-16 19:43:53 +0100

    shut up code warnings

--- ex3/Models/User.cs
+++ ex3/Models/User.cs
@@ -8,28 +8,28 @@ public class User
     /// <summary>
     /// The user's handle.
     /// </summary>
-    public string Username { get; set; }
+    public string Username { get; set; } = "";
     /// <summary>
     /// The password, stored as hashed bytes.
     /// </summary>
-    public byte[] Password { get; set; }
+    public byte[] Password { get; set; } = new byte[] { };
     /// <summary>
     /// The password salt, stored as raw bytes.
     /// </summary>
-    public byte[] Salt { get; set; }
+    public byte[] Salt { get; set; } = new byte[] { };
     /// <summary>
     /// The name of the hashing algorithm used. We keep this info around for compatibility with multiple algorithms.
     /// </summary>
-    public string Algorithm { get; set; }
+    public string Algorithm { get; set; } = "none";
 }
 public class RegisterData
 {
     /// <summary>
     /// The new user's username.
     /// </summary>
-    public string Username { get; set; }
+    public string Username { get; set; } = "";
     /// <summary>
     /// The plaintext password to register the user with.
     /// </summary>
-    public string Password { get; set; }
+    public string Password { get; set; } = "";
 }
