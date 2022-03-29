3d86745c3becf43cc0befdcb585a70651ec9b280
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-03-29 14:01:37 +0200

    symmetric method stubs

--- ex4/main.py
+++ ex4/main.py
@@ -1,3 +1,18 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+# Symmetric API stubs
+
+@app.get("/symmetric/key")
+async def sym_get_key():
+    return {"message": "This is a stub."}
+@app.post("/symmetric/key")
+async def sym_set_key():
+    return {"message": "This is a stub."}
+@app.post("/symmetric/encrypt")
+async def sym_encrypt():
+    return {"message": "This is a stub."}
+@app.post("/symmetric/decrypt")
+async def sym_decrypt():
+    return {"message": "This is a stub."}
